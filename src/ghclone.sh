aed5ce3336575a78235d4f08c25586e546eca2aa
Viktor Holta <viktor.holta@outlook.com>
2022-07-25 14:30:02 +0200

    Add better error-handling to ghclone

--- src/ghclone.sh
+++ src/ghclone.sh
@@ -4,13 +4,23 @@ OWNER=$1
 REPO=$2
 
 NORMAL="\033[0;0m"
-SUCCESS="\033[0;32m"
-ERROR="\033[0;31m"
+SUCCESS="\033[1;32m"
+ERROR="\033[1;31m"
 
-if [ $# != 2 ]
+# Check for right amount of arguments
+if [ $# -ne 2 ]
 then
-	echo -e "${ERROR}ERROR${NORMAL}: You need to specify a repository & repository owner."
-	echo -e "${ERROR}ERROR${NORMAL}: Try 'ghclone {owner} {repo}'"
+	echo -e "${ERROR}ERROR:${NORMAL} You need to specify an owner and their repository!"
+	echo "       Try \"ghclone [OWNER] [REPOSITORY]\"."
+	exit 1
 fi
 
-git clone git@github.com:$OWNER/$REPO.git
+# Try cloning
+if git clone "git@github.com:${OWNER}/${REPO}.git"
+then
+	echo -e "${SUCCESS}SUCCESS:${NORMAL} Cloned repository to ./${REPO}."
+	exit 0
+else
+	echo -e "${ERROR}ERROR:${NORMAL} Could not clone repository."
+	exit 1
+fi
